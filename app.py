2457da3a98ed7ed19695854975f7f57431883fa9
Jeongyounghyeon <jcs5150@naver.com>
2023-05-21 20:52:15 +0900

    use date param

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ def index():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    return render_template("result.html")
+    date = request.form.get('demo-name')
+    print("year : ", date[:4])
+    print("month : ", date[5:7])
+    print("day : ", date[8:10])
+
+	return render_template("result.html")
 
 @app.route('/method', methods=['GET', 'POST'])
 def method():
@@ -18,4 +23,4 @@ def method():
         return "POST로 전달"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
